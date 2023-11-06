e4f56f700b6d70438ff0717ee9cf64996801cac8
lorocks <lowell.lobo@hotmail.com>
2023-11-06 16:12:14 -0500

    Change published message
    This commit changes the published message from 'Hello World' to a custom
    message

--- src/cpp_pubsub/src/publisher_member_function.cpp
+++ src/cpp_pubsub/src/publisher_member_function.cpp
@@ -26,8 +26,8 @@ class MinimalPublisher : public rclcpp::Node
     void timer_callback()
     {
       auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      message.data = "Lowell's message number " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing message: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
