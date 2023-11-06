eaa8f1a59378c9884473734aca4e7b5f87d5c679
lorocks <lowell.lobo@hotmail.com>
2023-11-06 17:04:39 -0500

    Add node initialisation
    This commit initialises the node and spins the node

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -23,6 +23,8 @@ class MinimalSubscriber : public rclcpp::Node{
 
 
 int main (int argc, char * argv[]){
-    
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
     return 0;
 }
