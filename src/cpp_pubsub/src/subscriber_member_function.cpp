b338193f209dcdab599371ab2365da4722e98ae3
lorocks <lowell.lobo@hotmail.com>
2023-11-06 17:00:34 -0500

    Add Subscription implementaiton
    This commit adds the subscription implemenation class to read from the topic
    and display on the screen

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -1,3 +1,28 @@
-int main (){
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+class MinimalSubscriber : public rclcpp::Node{
+    public:
+        MinimalSubscriber()
+        : Node("minimal_subscriber"){
+            subscriber = this->create_subscription<std_msgs::msg::String>("topic", 10, std::bind(&MinimalSubscriber::timer_callback, this, std::placeholders::_1));
+            
+        }
+    private:
+        void timer_callback(const std_msgs::msg::String& msg ) const{
+            RCLCPP_INFO(this->get_logger(), "Received message: %s", msg.data.c_str());
+        }
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber;
+};
+
+
+int main (int argc, char * argv[]){
+    
     return 0;
 }
