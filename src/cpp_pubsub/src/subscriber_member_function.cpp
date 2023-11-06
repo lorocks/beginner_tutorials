7443a3ecbf34bb9cbe3911540cfa565122fae786
lorocks <lowell.lobo@hotmail.com>
2023-11-06 17:37:57 -0500

    Fix applicable cpplint issues
    This commit fixes all fixable cpplint errors

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -6,25 +6,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+class MinimalSubscriber : public rclcpp::Node {
+ public:
+  MinimalSubscriber() : Node("minimal_subscriber") {
+    subscriber = this->create_subscription<std_msgs::msg::String>(
+        "topic", 10,
+        std::bind(&MinimalSubscriber::timer_callback, this,
+                  std::placeholders::_1));
+  }
 
-class MinimalSubscriber : public rclcpp::Node{
-    public:
-        MinimalSubscriber()
-        : Node("minimal_subscriber"){
-            subscriber = this->create_subscription<std_msgs::msg::String>("topic", 10, std::bind(&MinimalSubscriber::timer_callback, this, std::placeholders::_1));
-            
-        }
-    private:
-        void timer_callback(const std_msgs::msg::String& msg ) const{
-            RCLCPP_INFO(this->get_logger(), "Received message: %s", msg.data.c_str());
-        }
-        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber;
+ private:
+  void timer_callback(const std_msgs::msg::String& msg) const {
+    RCLCPP_INFO(this->get_logger(), "Received message: %s", msg.data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber;
 };
 
-
-int main (int argc, char * argv[]){
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<MinimalSubscriber>());
-    rclcpp::shutdown();
-    return 0;
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
 }
