7ec70abe2611d857610653331233537fa5b2c796
lorocks <lowell.lobo@hotmail.com>
2023-11-07 19:08:31 -0500

    Add Doxygen syntax to code files
    This commit adds Doxygen syntax style documentation to generate documentation

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file subscriber_member_function.cpp
+ * @author Lowell Lobo
+ * @brief A minimal subscriber node for ros2 that posts the messages received in topic "/topic"
+ * @version 0.1
+ * @date 2023-11-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -6,8 +16,16 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+/**
+ * @brief MinimalSubscriber class that inherits form the Node class in rclcpp used to built a ros2 node
+ * 
+ */
 class MinimalSubscriber : public rclcpp::Node {
  public:
+ /**
+  * @brief Construct a new Minimal Subscriber object
+  *  Create a subscriber and read messages from topic "/topic"
+  */
   MinimalSubscriber() : Node("minimal_subscriber") {
     subscriber = this->create_subscription<std_msgs::msg::String>(
         "topic", 10,
@@ -16,12 +34,29 @@ class MinimalSubscriber : public rclcpp::Node {
   }
 
  private:
+ /**
+  * @brief A member function that logs the message received in "/topic" topic
+  * 
+  * @param msg Message from the topic "/topic"
+  */
   void timer_callback(const std_msgs::msg::String& msg) const {
     RCLCPP_INFO(this->get_logger(), "Received message: %s", msg.data.c_str());
   }
+
+  /**
+   * @brief Create a subscriber shared pointer from rclcpp to be used in the implementation
+   * 
+   */
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber;
 };
 
+/**
+ * @brief The main implementaiton of the code
+ * 
+ * @param argc Console input argument
+ * @param argv Console input argument
+ * @return int 
+ */
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
